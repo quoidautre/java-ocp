8d2adfd718bfcc0036d9cb59b22869f1c081c375
Fabrice Pestre <developpement.fabrice@riseup.net>
2017-08-25 17:11:24 +0200

    multiple exceptions with JAVA7

--- src/TestVille.java
+++ src/TestVille.java
@@ -5,16 +5,10 @@ public class TestVille {
         Ville v = null;
 
         try {
-            v = new Ville("Rddde", 12000, "France");
+            v = new Ville("rd", 12000, "France");
         }
-
-        //Gestion de l'exception sur le nombre d'habitants
-        catch (NombreHabitantException e) {
-            e.printStackTrace();
-        }
-
-        //Gestion de l'exception sur le nom de la ville
-        catch(NomVilleException e2){
+        //Gestion de plusieurs exceptions différentes
+        catch (NombreHabitantException | NomVilleException e2){
             System.out.println(e2.getMessage());
         }
         finally{
